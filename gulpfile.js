3e0cce6ca854ee3817754e3fdece3684d2372f84
Chris White <chris.white@xdesign.com>
2016-01-18 15:46:26 +0000

    Front-end building workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,59 @@
-var elixir = require('laravel-elixir');
+var gulp = require("gulp");
+var gulpif = require('gulp-if');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var jsmin = require('gulp-jsmin');
+var concat = require('gulp-concat');
+var minimist = require('minimist');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+var knownOptions = {
+    string: 'env',
+    default: {
+        env: process.env.NODE_ENV || 'dev'
+    }
+};
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+var options = minimist(process.argv.slice(2), knownOptions);
+
+var config = {
+    bootstrapDir: './node_modules/bootstrap-sass',
+    bourbonDir: './node_modules/bourbon',
+    scssDir: './resources/assets/sass',
+    jsDir: './resources/assets/js',
+    publicDir: './public'
+};
+
+gulp.task('sass', function() {
+    gulp.src(config.scssDir + '/main.scss')
+        .pipe(sass({
+            // Allow Twitter Bootstrap and Bourbon to be @import'ed
+            includePaths: [
+                config.bootstrapDir + '/assets/stylesheets',
+                config.bourbonDir + '/app/assets/stylesheets'
+            ]
+        }).on('error', sass.logError))
+        .pipe(gulpif(options.env === 'prod', cssmin()))
+        .pipe(gulp.dest(config.publicDir + '/dist/css'));
 });
+
+gulp.task('icons', function() {
+    // Copy Bootstrap's font files to the public directory
+    return gulp.src(config.bootstrapDir + '/assets/fonts/bootstrap/**/*')
+        .pipe(gulp.dest(config.publicDir + '/dist/fonts'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+            config.jsDir + '/app.js'
+        ])
+        .pipe(concat('app.js'))
+        .pipe(gulpif(options.env === 'prod', jsmin()))
+        .pipe(gulp.dest(config.publicDir + '/dist/js'));
+});
+
+gulp.task('watch', function() {
+    // Recompile SASS on changes
+    gulp.watch(config.scssDir + '/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'icons', 'js', 'watch']);
---
+++ resources/assets/js/app.js
@@ -0,0 +1,7 @@
+/*
+ * The main JavaScript file for small, generic JavaScript snippets that apply to the entire application.
+ * Do not be afraid to split JavaScript functionality up into multiple files in this directory. They all get combined
+ * into public/dist/js/app.js by Gulp.
+ */
+
+$('test').on('click');
