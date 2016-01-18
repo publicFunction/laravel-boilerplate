236833ecec4eb79569c6a87a9a0d25453a10240d
Chris White <chris.white@xdesign.com>
2016-01-18 15:48:46 +0000

    Remove useless code

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -3,5 +3,3 @@
  * Do not be afraid to split JavaScript functionality up into multiple files in this directory. They all get combined
  * into public/dist/js/app.js by Gulp.
  */
-
-$('test').on('click');
