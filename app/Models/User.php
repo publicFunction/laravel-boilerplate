b48bc7304b24bab6dabc13a538ecf5b838c236b6
Chris White <chris.white@xdesign.com>
2016-01-19 16:14:53 +0000

    Update User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,15 +12,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
+        'name', 'email', 'password'
     ];
 }
